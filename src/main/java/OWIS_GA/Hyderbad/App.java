4fc6a0116302d08a8e705d8a21c79af752c12bbd
Virendram123 <virendra@giftechnologies.com>
2024-01-09 16:13:16 +0530

    Demo project

---
+++ src/main/java/OWIS_GA/Hyderbad/App.java
@@ -0,0 +1,13 @@
+package OWIS_GA.Hyderbad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
